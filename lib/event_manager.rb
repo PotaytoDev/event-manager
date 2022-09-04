0a43da26ff2709df8960b4e2d70f257db48bcdf3
Potayto <p.o.tayto@outlook.com>
2022-09-04 15:56:49 -0700

    Read names and zip codes of attendees from csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,28 @@
-puts "Event Manager Initialized!"
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode ||= '0'
+
+  if zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "Name: #{name}, Zip Code: #{zipcode}"
+end
