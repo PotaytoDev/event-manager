dd41aec0e9fbc507a679f20afc5c76817d16c789
Potayto <p.o.tayto@outlook.com>
2022-09-05 17:39:41 -0700

    Get names of legislators that represent each attendee
    The names of the legislators are acquired through the use of Google's
    Civic Information API.
    
    Because of the way that the API handles errors, the tutorial I am
    following no longer works as intended in handling the records that are
    missing data. Due to this, it was necessary to redirect the output
    stream to a new StringIO object before the API calls are made, and then
    substitute back the default output stream after the calls are made,
    which makes it so that the errors are no longer output to the console.
    
    This solution was provided by a user in The Odin Project's discord server:
    https://discord.com/channels/505093832157691914/543074220691947531/1008539799549919314

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 # Make any zip code into a five-digit zip code
 def clean_zipcode(zipcode)
@@ -6,6 +7,9 @@ def clean_zipcode(zipcode)
 end
 
 puts 'Event Manager Initialized!'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -17,5 +21,25 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "Name: #{name}, Zip Code: #{zipcode}"
+  stdout = $stdout
+
+  begin
+    $stdout = StringIO.new
+
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+
+    legislator_names = legislators.officials.map(&:name)
+    legislators_string = legislator_names.join(', ')
+  rescue
+    legislators_string = 'You can find your representatives by ' \
+    'visiting www.commoncause.org/take-action/find-elected-officials'
+  ensure
+    $stdout = stdout
+  end
+
+  puts "Name: #{name}, Zip Code: #{zipcode}, Legislators: #{legislators_string}"
 end
